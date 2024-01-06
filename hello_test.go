b1d8ab7074d301fae406ccb3f0e6bc2332355ff8
cdsiats <118641404+cdsiats@users.noreply.github.com>
2024-01-06 13:09:41 +0800

    refactor test and code

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Christian")
-	want := "Hello, Christian"
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Christian")
+		want := "Hello, Christian"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say hello world when there are no args", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Wrold"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
